86a8fc1dacc6ba3b50bc17414c724005e0de30c5
Aliaksandr Kalbushkin <akolbushkin@tula.co>
2023-07-06 18:10:20 +0300

    stripe publishable key

--- src/utils/stripe/stripe.utils.ts
+++ src/utils/stripe/stripe.utils.ts
@@ -1,5 +1,5 @@
 import { loadStripe } from '@stripe/stripe-js';
 
 export const stripePromise = loadStripe(
-  'pk_test_51NN9JdFFFlG7YE1WnGKSGwCx6pj7DG862HBscp7H0lbNfHF7Rfdp7Z4LrRe21JlMauHtiaQl19InUVQb8QXoYr7300tjyoyj46'
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 );
